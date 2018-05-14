9500f59be894967139efb2afa15d984f2d206b7f
Alex Moreno <aem.gamboa@gmail.com>
2018-05-14 18:44:33 -0400

    hgjgjhkgjhk

--- BeneficiosTarjetas_Web/Config.cs
+++ BeneficiosTarjetas_Web/Config.cs
@@ -5,5 +5,18 @@ namespace BeneficiosTarjetas_Web
     {
         public static int ElementWaitngTimeout = 5;
         public static string BaseURL = "http://www.beneficiostarjetas.cl";
+
+        public static class Credenciales
+        {
+            public static class Valid
+            {
+                public static string correo = "pruebaingesmart@gmail.com";
+                public static string password = "123qwe";
+            }
+
+            public static 
+        }
+
+
     }
 }
